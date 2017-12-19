90bfc2172a59e86ebff27ac16a082c27cf094beb
rubydiarmit <34691001+rubydiarmit@users.noreply.github.com>
2017-12-19 14:45:26 -0500

    Add files via upload

---
+++ BalanceCalculatorProject.py
@@ -0,0 +1,184 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+n%10
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+9
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+9
+29
+1
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+0
+1
+4
+9
+16
+25
+36
+49
+64
+81
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+1
+1
+4
+27
+256
+3125
+46656
+823543
+16777216
+387420489
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+I will not chew gum in class
+0
+1
+4
+9
+16
+25
+36
+49
+64
+81
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+2
+2
+4
+4
+6
+6
+8
+8
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+2
+4
+6
+8
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+welcome to my calculator
+Year 0 Your total balance i 1000
+Year 1 Your total balance i 1000
+Year 2 Your total balance i 1000
+Year 3 Your total balance i 1000
+Year 4 Your total balance i 1000
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+welcome to my calculator
+Year 0 Your total balance i 1000
+Year 1 Your total balance i 1050.0
+Year 2 Your total balance i 1102.5
+Year 3 Your total balance i 1157.625
+Year 4 Your total balance i 1215.50625
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+welcome to my calculator
+Year 0 Your total balance is 1000.00
+Year 1 Your total balance is 1050.00
+Year 2 Your total balance is 1102.50
+Year 3 Your total balance is 1157.62
+Year 4 Your total balance is 1215.51
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+welcome to my calculator
+Year 0 Your total balance is 500000.00
+Year 1 Your total balance is 510000.00
+Year 2 Your total balance is 520200.00
+Year 3 Your total balance is 530604.00
+Year 4 Your total balance is 541216.08
+Year 5 Your total balance is 552040.40
+Year 6 Your total balance is 563081.21
+Year 7 Your total balance is 574342.83
+Year 8 Your total balance is 585829.69
+Year 9 Your total balance is 597546.28
+Year 10 Your total balance is 609497.21
+Year 11 Your total balance is 621687.15
+Year 12 Your total balance is 634120.90
+Year 13 Your total balance is 646803.32
+Year 14 Your total balance is 659739.38
+Year 15 Your total balance is 672934.17
+Year 16 Your total balance is 686392.85
+Year 17 Your total balance is 700120.71
+Year 18 Your total balance is 714123.12
+Year 19 Your total balance is 728405.59
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+welcome to my calculator
+Original balance: 10000
+Interest Rate: 0.05
+Years: 10
+Traceback (most recent call last):
+  File "/Users/Guest/Documents/Untitled.py", line 13, in <module>
+    for i in range(years):
+TypeError: 'float' object cannot be interpreted as an integer
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+welcome to my calculator
+Original balance: 10000
+Interest Rate: 0.05
+Years: 10
+Year 0 Your total balance is 10000.00
+Year 1 Your total balance is 10500.00
+Year 2 Your total balance is 11025.00
+Year 3 Your total balance is 11576.25
+Year 4 Your total balance is 12155.06
+Year 5 Your total balance is 12762.82
+Year 6 Your total balance is 13400.96
+Year 7 Your total balance is 14071.00
+Year 8 Your total balance is 14774.55
+Year 9 Your total balance is 15513.28
+>>> 
